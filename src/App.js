4e7dba09956151d33a17dca7fd97cf47dcb34761
Denis Pankov <idenispankov@gmail.com>
2021-03-13 15:23:32 -0800

    json-server

--- src/App.js
+++ src/App.js
@@ -5,26 +5,7 @@ import AddTask from './components/AddTask';
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at school',
-      day: 'Feb 6th at 1:30pm',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Food shopping',
-      day: 'Feb 5th at 2:30pm',
-      reminder: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   function addTaskClick() {
     setShowAddTask(!showAddTask);
