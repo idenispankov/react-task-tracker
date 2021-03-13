c6ba655cdc1750c09233b68b36d917a95050ad3a
Denis Pankov <idenispankov@gmail.com>
2021-03-12 23:04:26 -0800

    added styles to index.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,9 @@ import PropTypes from 'prop-types';
 
 const Header = ({ title }) => {
   return (
-    <header>
+    <header className='header'>
       <h1>{title}</h1>
+      <button className='btn'>Add</button>
     </header>
   );
 };
