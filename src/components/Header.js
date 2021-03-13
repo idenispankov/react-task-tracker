b95375c3a803335db09c5247a8e8423f324e2fcc
Denis Pankov <idenispankov@gmail.com>
2021-03-12 23:25:51 -0800

    Button Component added - learning dealing with onClick passing it as a prop

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,16 @@
-import PropTypes from 'prop-types';
+import Button from './Button';
 
 const Header = ({ title }) => {
+  function addItemClick() {
+    console.log('Click!!!');
+  }
+
   return (
     <header className='header'>
-      <h1>{title}</h1>
-      <button className='btn'>Add</button>
+      <h1>Task Tracker</h1>
+      <Button color='green' text='Add' onClick={addItemClick} />
     </header>
   );
-};
-
-Header.defaultProps = {
-  title: 'Task Tracker',
-};
-
-Header.propTypes = {
-  title: PropTypes.string.isRequired,
 };
 
 export default Header;
